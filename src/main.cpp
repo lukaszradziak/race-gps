b60e27cb9222d8da74d4a875040b6d92fd8ad3dc
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-09-24 01:28:33 +0200

    Sample BLE server

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <Arduino.h>
 #include <TinyGPSPlus.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
 const char *gpsStream =
   "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
@@ -12,38 +24,79 @@ const char *gpsStream =
 TinyGPSPlus gps;
 byte data[8];
 
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  };
+
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello in Race GPS");
   Serial.println();
 
-  while (*gpsStream) {
-    if (gps.encode(*gpsStream++)) {
-      int time = gps.time.value();
-      int satellites = gps.satellites.value();
-      int speed = gps.speed.knots();
-      int alt = gps.altitude.meters();
+  BLEDevice::init("ESP32");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ   |
+    BLECharacteristic::PROPERTY_WRITE  |
+    BLECharacteristic::PROPERTY_NOTIFY |
+    BLECharacteristic::PROPERTY_INDICATE
+  );
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
 
-      data[0] = (alt >> 8) & 0xff;
-      data[1] = alt & 0xff;
-      data[2] = speed & 0xff;
-      data[3] = satellites & 0xff;
-      data[4] = (time >> 24) & 0xff;
-      data[5] = (time >> 16) & 0xff;
-      data[6] = (time >> 8) & 0xff;
-      data[7] = time & 0xff;
-
-      for (int i = 0; i < 8; i++) {
-        Serial.print(data[i], HEX);
-        Serial.print(" ");
-      }
-
-      Serial.println();
-      delay(100);
-    }
-  }
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);
+  BLEDevice::startAdvertising();
 }
 
 void loop() {
-  //
+  if (deviceConnected) {
+    delay(1000);
+
+    while (*gpsStream) {
+      if (gps.encode(*gpsStream++)) {
+        int time = gps.time.value();
+        int satellites = gps.satellites.value();
+        int speed = gps.speed.knots();
+        int alt = gps.altitude.meters();
+
+        data[0] = (alt >> 8) & 0xff;
+        data[1] = alt & 0xff;
+        data[2] = speed & 0xff;
+        data[3] = satellites & 0xff;
+        data[4] = (time >> 24) & 0xff;
+        data[5] = (time >> 16) & 0xff;
+        data[6] = (time >> 8) & 0xff;
+        data[7] = time & 0xff;
+
+        pCharacteristic->setValue(data, 8);
+        pCharacteristic->notify();
+        delay(100);
+      }
+    }
+
+    ESP.restart();
+  }
+
+  if (!deviceConnected && oldDeviceConnected) {
+    delay(500);
+    pServer->startAdvertising();
+    Serial.println("start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+
+  if (deviceConnected && !oldDeviceConnected) {
+    oldDeviceConnected = deviceConnected;
+  }
 }
