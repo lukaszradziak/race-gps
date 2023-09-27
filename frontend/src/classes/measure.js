78160a83cc74f364496bc40fb6bf9030ccd1fdb6
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-09-28 01:33:49 +0200

    Add interpolate and searching speed

--- frontend/src/classes/measure.js
+++ frontend/src/classes/measure.js
@@ -35,7 +35,7 @@ export class Measure {
         configRow.ready = false;
       }
 
-      if (!configRow.started && configRow.ready && speed > configRow.start) {
+      if (!configRow.started && configRow.ready && speed > configRow.start + 1) {
         configRow.started = true;
         configRow.records = [];
         configRow.records.push(record);
@@ -46,12 +46,52 @@ export class Measure {
       } else if (configRow.started && speed > configRow.end) {
         configRow.started = false;
         configRow.records.push(record);
-        this.onNewResult(configRow);
+        this.onNewResult(
+          this.parseResult(configRow)
+        );
       } else if (configRow.started && speed > configRow.start && speed < configRow.end) {
         configRow.records.push(record);
       }
     }
 
     this.records.push(record);
+  }
+
+  parseResult (configRow) {
+    const records = [...configRow.previousRecords, ...configRow.records];
+    const speedTime = {};
+
+    for (let i = configRow.start; i <= configRow.end; i += 10) {
+      speedTime[i] = this.findSpeed(i, records);
+      console.log('> interpolateResult', i, speedTime[i]);
+      console.log('---');
+    }
+
+    configRow.measureTime = ((speedTime[configRow.end] - speedTime[configRow.start]) / 100).toFixed(2);
+
+    return configRow;
+  }
+
+  findSpeed (speed, records) {
+    let interpolateResult = 0;
+
+    for (const index in records) {
+      const previousRecord = records[parseInt(index) - 1];
+      const actualRecord = records[index];
+
+      if (!previousRecord) {
+        continue;
+      }
+
+      if (actualRecord.speed > speed && previousRecord.speed <= speed) {
+        console.log('> foundSpeed: ', previousRecord.speed, ' - ', actualRecord.speed);
+        interpolateResult = actualRecord.time -
+          ((actualRecord.time - previousRecord.time) *
+            (actualRecord.speed - speed) /
+            (actualRecord.speed - previousRecord.speed));
+      }
+    }
+
+    return interpolateResult;
   }
 }
