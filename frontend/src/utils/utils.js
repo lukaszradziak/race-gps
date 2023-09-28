3bf6792bff0c0ab67950b474a033742718db2efe
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-09-28 15:02:24 +0200

    Add download to measure

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,4 @@
-export function downloadCSV (str, name = '') {
+export function downloadFile (str, name = '') {
   const hiddenElement = document.createElement('a');
   hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(str);
   hiddenElement.target = '_blank';
