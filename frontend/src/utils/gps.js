fa02e10f8f8d50783f0ee3695ac73eca6b302a77
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-09-28 20:51:42 +0200

    More accuracy in gps speed

--- frontend/src/utils/gps.js
+++ frontend/src/utils/gps.js
@@ -6,10 +6,10 @@ export function parseGpsData (value) {
       value.getUint8(7) * Math.pow(256, 1),
       value.getUint8(8) * Math.pow(256, 0)
     ].reduce((a, b) => a + b, 0),
-    speed: ([
+    speed: [
       value.getUint8(2) * Math.pow(256, 1),
       value.getUint8(3) * Math.pow(256, 0)
-    ].reduce((a, b) => a + b, 0) / 100 * 1.852).toFixed(4),
+    ].reduce((a, b) => a + b, 0) / 100 * 1.852,
     alt: [
       value.getUint8(0) * Math.pow(256, 1),
       value.getUint8(1) * Math.pow(256, 0)
