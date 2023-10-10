d77cd773320eb4f7eaacab8f85336a1bd558f373
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-10-10 22:56:12 +0200

    Add modal with times

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,14 +1,24 @@
-import { ReactNode } from "react";
+import { ComponentPropsWithoutRef } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface CardOptions {
+  title?: string;
+}
 
 export function Card({
   title,
   children,
-}: {
-  title?: string;
-  children: ReactNode;
-}) {
+  className,
+  ...props
+}: ComponentPropsWithoutRef<"div"> & CardOptions) {
   return (
-    <div className="overflow-hidden bg-white rounded-lg shadow mb-2">
+    <div
+      className={twMerge(
+        `overflow-hidden bg-white rounded-lg shadow mb-2`,
+        className,
+      )}
+      {...props}
+    >
       {title ? (
         <div className="border-b border-gray-200 bg-white px-4 py-5 sm:px-6">
           <h3 className="text-lg font-medium leading-6 text-gray-900">
