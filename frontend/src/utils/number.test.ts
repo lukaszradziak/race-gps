45222728a5fb6646444ea6db5461b991973393b1
Łukasz Radziak <lukasz.radziak@icloud.com>
2023-10-27 01:41:51 +0200

    Add next tests

--- frontend/src/utils/number.test.ts
+++ frontend/src/utils/number.test.ts
@@ -1,13 +1,38 @@
 import { describe, expect, test } from "vitest";
-import { parseToTime } from "./number.ts";
+import { actualTime, averageValues, parseToTime } from "./number.ts";
 
-describe("number", () => {
-  test("parseToTime1", () => {
+describe("actualTime", () => {
+  test("test length of time string", () => {
+    expect(actualTime().length).toBe(8);
+  });
+});
+
+describe("averageValues", () => {
+  test("test average result", () => {
+    expect(
+      averageValues(
+        [
+          0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85,
+          90, 95, 100,
+        ],
+        13,
+        8,
+      ),
+    ).toBe(58.8235294117647);
+  });
+});
+
+describe("parseToTime", () => {
+  test("test length validation", () => {
+    expect(() => parseToTime("100")).toThrowError('Wrong value of time: "100"');
+  });
+
+  test("test time difference", () => {
     expect(parseToTime("15511020") - parseToTime("15511000")).toBe(20);
     expect(parseToTime("15511020") - parseToTime("15505960")).toBe(1060);
   });
 
-  test("parseToTime2", () => {
+  test("test change day", () => {
     const sampleData: string[] = [
       "23592000",
       "23593000",
