d7fb71f3ecb4979a49a2784da4d65dff491d0b87
TysiacSzescset <drw1@op.pl>
2023-11-16 01:00:02 +0100

    Dyno smoothing and visual

--- frontend/src/utils/number.ts
+++ frontend/src/utils/number.ts
@@ -1,8 +1,8 @@
 export function actualTime() {
   return parseInt(
     new Date().toLocaleTimeString().split(":").join("") +
-      "" +
-      (new Date().getMilliseconds() / 10).toFixed(0).padStart(2, "0"),
+    "" +
+    (new Date().getMilliseconds() / 10).toFixed(0).padStart(2, "0"),
   );
 }
 
@@ -21,4 +21,26 @@ export function averageValues(
       }, 0) /
     (offset * 2 + 1)
   );
+}
+
+export function weightedAverageValues(
+  values: number[],
+  index: number,
+  matrix = [
+    { idx: -2, w: 0.4 },
+    { idx: -1, w: 0.6 },
+    { idx: 0, w: 1 },
+    { idx: 1, w: 0.6 },
+    { idx: 2, w: 0.4 }
+  ]
+) {
+  let pointsSum = 0;
+  let pointsDiv = 0;
+  matrix.forEach(avgPoint => {
+    if (index + avgPoint.idx >= 0 && index + avgPoint.idx < values.length) {
+      pointsSum += values[index + avgPoint.idx] * avgPoint.w;
+      pointsDiv += avgPoint.w;
+    }
+  });
+  return pointsSum / (pointsDiv > 0 ? pointsDiv : 1);
 }
