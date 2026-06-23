9845ef0bd2f515fa208002b2274e66aefd0c7879
Łukasz Radziak <lukasz.radziak@icloud.com>
2026-06-23 13:15:01 +0200

    Clear project for v2

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Race GPS v2</h1>
+          <p>
+            <code>Hello world!</code>
+          </p>
+        </div>
+
+      </section>
+    </>
+  )
+}
+
+export default App
